f0ccc4be276c5d8d5c894224de87424838c8756e
Elias <elias.rhoden@hotmail.com>
2024-10-13 22:10:45 +0200

    no need for a class

--- tf_freq_id/tf_id.py
+++ tf_freq_id/tf_id.py
@@ -22,100 +22,88 @@ def d2c(Gd, Ts):
     return ctrl.ss(ac, bc, cc, dc)
 
 
+def fit(w, Y, dt, model_order=None, order_tol=0.1):
+    """
+    Fit a transfer function model to frequency response data
+    """
 
-class TfIdent:
+    if model_order is None:
+        for i in range(10):
+            Ge, error = self._fit_tf(w,Y, dt, i, order_tol)
+            if error < order_tol:
+                break
+    else:
+        Ge, error = self._fit_tf(w,Y, dt, model_order, order_tol)
 
-    def __init__(self):
-
-        pass 
-        # todo
-
-
-
-
-
-    def fit(self, w, Y, dt, model_order=None, order_tol=0.1):
-        """
-        Fit a transfer function model to frequency response data
-        """
-
-        if model_order is None:
-            for i in range(10):
-                Ge, error = self._fit_tf(w,Y, dt, i, order_tol)
-                if error < order_tol:
-                    break
-        else:
-            Ge, error = self._fit_tf(w,Y, dt, model_order, order_tol)
-
-        return Ge, error
+    return Ge, error
 
 
 
-    def _fit_tf(self, w, Y, dt, model_order, order_tol):
+def _fit_tf(w, Y, dt, model_order, order_tol):
 
-        n = model_order
+    n = model_order
 
-        M = []
-        Y = []
+    M = []
+    Y = []
 
-        for i,yi in enumerate(GY):
+    for i,yi in enumerate(GY):
 
-            Ri = mag[i]
-            phi = phase[i]
-            wk = w[i]   
+        Ri = mag[i]
+        phi = phase[i]
+        wk = w[i]   
 
-            Oi = wk*Ts
+        Oi = wk*Ts
 
-            row1 = []
+        row1 = []
 
-            # Real part
+        # Real part
 
-            #R1 = Ri*(np.cos(phi) + np.cos(phi - Oi)*a[1] + np.cos(phi - 2*Oi)*a[2])
-            #R2 = b[0] + np.cos(-Oi)*b[1] + np.cos(-2*Oi)*b[2]
+        #R1 = Ri*(np.cos(phi) + np.cos(phi - Oi)*a[1] + np.cos(phi - 2*Oi)*a[2])
+        #R2 = b[0] + np.cos(-Oi)*b[1] + np.cos(-2*Oi)*b[2]
 
-            for i in range(1,n):
-                row1.append(-Ri*np.cos(phi - Oi*i))
+        for i in range(1,n):
+            row1.append(-Ri*np.cos(phi - Oi*i))
 
-            for i in range(0,n):
-                if i == 0:
-                    row1.append(1.0)
-                else:
-                    row1.append(np.cos(-Oi*i))
+        for i in range(0,n):
+            if i == 0:
+                row1.append(1.0)
+            else:
+                row1.append(np.cos(-Oi*i))
 
-            row2 = []
-            # Imag part
-            #I1 = Ri*(np.sin(phi) + np.sin(phi - Oi)*a[1] + np.sin(phi - 2*Oi)*a[2])
-            #I2 = np.sin(-Oi)*b[1] + np.sin(-2*Oi)*b[2] # NO b0!
+        row2 = []
+        # Imag part
+        #I1 = Ri*(np.sin(phi) + np.sin(phi - Oi)*a[1] + np.sin(phi - 2*Oi)*a[2])
+        #I2 = np.sin(-Oi)*b[1] + np.sin(-2*Oi)*b[2] # NO b0!
 
-            for i in range(1,n):
-                row2.append(-Ri*np.sin(phi - Oi*i))
+        for i in range(1,n):
+            row2.append(-Ri*np.sin(phi - Oi*i))
 
-            for i in range(0,n):
-                if i == 0:
-                    row2.append(0.0)
-                else:
-                    row2.append(np.sin(-Oi*i))
+        for i in range(0,n):
+            if i == 0:
+                row2.append(0.0)
+            else:
+                row2.append(np.sin(-Oi*i))
 
-            M.append(row1)
-            M.append(row2)
+        M.append(row1)
+        M.append(row2)
 
-            Y.append(Ri*np.cos(phi))
-            Y.append(Ri*np.sin(phi))
+        Y.append(Ri*np.cos(phi))
+        Y.append(Ri*np.sin(phi))
 
-        sol = np.linalg.lstsq(M,Y,rcond=None)
-        theta = sol[0]
-        theta
+    sol = np.linalg.lstsq(M,Y,rcond=None)
+    theta = sol[0]
+    theta
 
-        a = theta[0:n-1]
-        b = theta[n-1:]
+    a = theta[0:n-1]
+    b = theta[n-1:]
 
-        a = np.r_[1,a]
+    a = np.r_[1,a]
 
-        Ge = ctrl.tf(b,a,Ts)
-        Ge = ctrl.minreal(Ge,tol=0.1)
+    Ge = ctrl.tf(b,a,Ts)
+    Ge = ctrl.minreal(Ge,tol=0.1)
 
-        Y_mdl = ctrl.freqresp(Ge,w)
+    Y_mdl = ctrl.freqresp(Ge,w)
 
-        error = np.mean(np.abs(Y - Y_mdl))
+    error = np.mean(np.abs(Y - Y_mdl))
 
-        return Ge, error
+    return d2c(Ge,Ts), error
